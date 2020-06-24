86a5a1b97200d3b6d61e7dac2fa91dbb2365b403
Ricardo Cruz <piradoiv@gmail.com>
2020-06-24 16:48:41 +0100

    Añade una exclamación al final

--- Rack/server.rb
+++ Rack/server.rb
@@ -2,7 +2,7 @@ require 'rack'
 require '../Handler/prack.rb'
 
 app = Proc.new do |env|
-    ['200', {'Content-Type' => 'text/html'}, ['A barebones rack app.']]
+    ['200', {'Content-Type' => 'text/html'}, ['A barebones rack app!']]
 end
 
 Rack::Handler::Prack.run app
